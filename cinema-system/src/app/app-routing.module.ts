5f67ece898dbbdd3a5998dcdacc7a977e4fef551
Admin <luongsexy007@gmail.com>
2020-04-25 21:02:01 +0700

    be backup

--- cinema-system/src/app/app-routing.module.ts
+++ cinema-system/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import { TicketBookingInformationComponent } from './ticket-booking-information/ticket-booking-information.component';
+import { PositionSelectorComponent } from './position-selector/position-selector.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'chon-ghe', component: PositionSelectorComponent },
+  { path: 'thong-tin-dat-ve', component: TicketBookingInformationComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
--- cinema-system/src/app/app.module.ts
+++ cinema-system/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +15,13 @@ import { HttpClientModule } from '@angular/common/http';
     PositionSelectorComponent,
     TicketBookingInformationComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
